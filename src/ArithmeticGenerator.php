458a07aa869291083618da8e634b2f234541678d
Kevin Crawley <kcrawley1202@gmail.com>
2013-08-21 18:19:23 -0500

    implemented arithmetic generation

--- src/ArithmeticGenerator.php
+++ src/ArithmeticGenerator.php
@@ -4,13 +4,29 @@ use CaptchaGen\Interfaces\GeneratorInterface;
 
 class ArithmeticGenerator implements GeneratorInterface
 {
+    private $config;
+    private $imaging;
+
     public function __construct(Config $config, Imaging $imaging)
     {
-
+        $this->config = $config;
+        $this->imaging = $imaging;
     }
 
     public function build()
     {
-        // TODO: Implement build() method.
+        $int_a = rand(1,9);
+        $int_b = rand(1,9);
+
+        if ($int_a >= $int_b) {
+            $challenge = $int_a.' - '.$int_b.' = x';
+            $response = $int_a - $int_b;
+        } else {
+            $challenge = $int_a.' + '.$int_b.' = x';
+            $response = $int_a + $int_b;
+        }
+
+        $this->config->setKey('challenge', $challenge);
+        $this->config->setKey('response', $response);
     }
 }
